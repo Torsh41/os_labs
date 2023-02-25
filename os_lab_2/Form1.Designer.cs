9c19aca87450ad72fa38d56a51d7faa88573c8d4
Torsher41k <kola9876543210@mail.ru>
2023-02-25 19:12:56 +0300

    this time final for shure

--- os_lab_2/Form1.Designer.cs
+++ os_lab_2/Form1.Designer.cs
@@ -30,22 +30,30 @@ namespace os_lab_2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.button_copy = new System.Windows.Forms.Button();
+            this.button_move = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
+            this.button_file_1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button_folder_1 = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button5 = new System.Windows.Forms.Button();
+            this.button_file_2 = new System.Windows.Forms.Button();
+            this.button_folder_2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button_rename = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_exchange = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label2 - Source
+            // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(188, 173);
@@ -54,7 +62,113 @@ namespace os_lab_2
             this.label2.TabIndex = 4;
             this.label2.Text = "Source";
             // 
-            // label3 - Destination
+            // button_copy
+            // 
+            this.button_copy.Location = new System.Drawing.Point(338, 187);
+            this.button_copy.Name = "button_copy";
+            this.button_copy.Size = new System.Drawing.Size(75, 23);
+            this.button_copy.TabIndex = 5;
+            this.button_copy.Text = "copy";
+            this.button_copy.UseVisualStyleBackColor = true;
+            this.button_copy.Click += new System.EventHandler(this.button_copy_click);
+            // 
+            // button_move
+            // 
+            this.button_move.Location = new System.Drawing.Point(338, 245);
+            this.button_move.Name = "button_move";
+            this.button_move.Size = new System.Drawing.Size(75, 23);
+            this.button_move.TabIndex = 6;
+            this.button_move.Text = "move";
+            this.button_move.UseVisualStyleBackColor = true;
+            this.button_move.Click += new System.EventHandler(this.button_move_click);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(222, 276);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(75, 23);
+            this.button_delete.TabIndex = 7;
+            this.button_delete.Text = "delete";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // button_file_1
+            // 
+            this.button_file_1.Location = new System.Drawing.Point(177, 5);
+            this.button_file_1.Name = "button_file_1";
+            this.button_file_1.Size = new System.Drawing.Size(75, 23);
+            this.button_file_1.TabIndex = 9;
+            this.button_file_1.Text = "Файл";
+            this.button_file_1.UseVisualStyleBackColor = true;
+            this.button_file_1.Click += new System.EventHandler(this.button_file_1_click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button_folder_1);
+            this.panel1.Controls.Add(this.button_file_1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(77, 197);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 60);
+            this.panel1.TabIndex = 10;
+            // 
+            // button_folder_1
+            // 
+            this.button_folder_1.Location = new System.Drawing.Point(177, 34);
+            this.button_folder_1.Name = "button_folder_1";
+            this.button_folder_1.Size = new System.Drawing.Size(75, 23);
+            this.button_folder_1.TabIndex = 10;
+            this.button_folder_1.Text = "Папка";
+            this.button_folder_1.Click += new System.EventHandler(this.button_folder_1_click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button_file_2);
+            this.panel2.Controls.Add(this.button_folder_2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Location = new System.Drawing.Point(419, 197);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(255, 60);
+            this.panel2.TabIndex = 11;
+            // 
+            // button_file_2
+            // 
+            this.button_file_2.Location = new System.Drawing.Point(177, 3);
+            this.button_file_2.Name = "button_file_2";
+            this.button_file_2.Size = new System.Drawing.Size(75, 23);
+            this.button_file_2.TabIndex = 11;
+            this.button_file_2.Text = "Файл";
+            this.button_file_2.Click += new System.EventHandler(this.button_file_2_click);
+            // 
+            // button_folder_2
+            // 
+            this.button_folder_2.Location = new System.Drawing.Point(177, 32);
+            this.button_folder_2.Name = "button_folder_2";
+            this.button_folder_2.Size = new System.Drawing.Size(75, 23);
+            this.button_folder_2.TabIndex = 9;
+            this.button_folder_2.Text = "Папка";
+            this.button_folder_2.UseVisualStyleBackColor = true;
+            this.button_folder_2.Click += new System.EventHandler(this.button_folder_2_click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 20);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "C:\\Users\\nick\\Pictures\\Camera Roll";
+            // 
+            // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(507, 172);
@@ -63,103 +177,66 @@ namespace os_lab_2
             this.label3.TabIndex = 12;
             this.label3.Text = "Destination";
             // 
-            // button1 - Copy
+            // button_rename
             // 
-            this.button1.Location = new System.Drawing.Point(338, 197);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "copy";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_rename.Location = new System.Drawing.Point(117, 276);
+            this.button_rename.Name = "button_rename";
+            this.button_rename.Size = new System.Drawing.Size(75, 23);
+            this.button_rename.TabIndex = 13;
+            this.button_rename.Text = "rename";
             // 
-            // button2 - Move
+            // textBox3
             // 
-            this.button2.Location = new System.Drawing.Point(338, 234);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "move";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.textBox3.Location = new System.Drawing.Point(432, 313);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 20);
+            this.textBox3.TabIndex = 14;
             // 
-            // button3 - Delete
+            // label1
             // 
-            this.button3.Location = new System.Drawing.Point(172, 275);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "delete";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(481, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "input name";
             // 
-            // button4 - Source
+            // button_exchange
             // 
-            this.button4.Location = new System.Drawing.Point(177, 18);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "Файл";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button_exchange.Location = new System.Drawing.Point(339, 217);
+            this.button_exchange.Name = "button_exchange";
+            this.button_exchange.Size = new System.Drawing.Size(75, 23);
+            this.button_exchange.TabIndex = 16;
+            this.button_exchange.Text = "⇄";
+            this.button_exchange.Click += new System.EventHandler(this.button_exchange_click);
             // 
-            // button5 - Destination
+            // textBox4
             // 
-            this.button5.Location = new System.Drawing.Point(177, 18);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 9;
-            this.button5.Text = "Файл";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // textBox1 - Source
-            // 
-            this.textBox1.Location = new System.Drawing.Point(3, 20);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Text = "C:\\Users\\nick\\Pictures\\Saved Pictures\\water-nature-bird-wing-stone-summer-pond-wildlife-reflection-beak-fauna-poultry-drake-one-duck-vertebrate-beautiful-waterfowl-water-bird-mallard-canard-mergus-ducks-geese-and-swans-seaduck-614686.jpg";
-            this.textBox1.ReadOnly = false;
-            this.textBox1.Size = new System.Drawing.Size(168, 20);
-            this.textBox1.TabIndex = 8;
-            // 
-            // textBox2 - Destination
-            // 
-            this.textBox2.Location = new System.Drawing.Point(3, 20);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Text = "C:\\Users\\nick\\Pictures\\Camera Roll";
-            this.textBox2.ReadOnly = false;
-            this.textBox2.Size = new System.Drawing.Size(168, 20);
-            this.textBox2.TabIndex = 8;
-            // 
-            // panel1 - Source
-            // 
-            this.panel1.Controls.Add(this.button4);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Location = new System.Drawing.Point(77, 197);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(255, 60);
-            this.panel1.TabIndex = 10;
-            // 
-            // panel2 - Destination
-            // 
-            this.panel2.Controls.Add(this.button5);
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Location = new System.Drawing.Point(419, 197);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(255, 60);
-            this.panel2.TabIndex = 11;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(77, 1);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(595, 168);
+            this.textBox4.TabIndex = 18;
+            this.textBox4.Text = resources.GetString("textBox4.Text");
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(760, 427);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button_exchange);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button_rename);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.button_move);
+            this.Controls.Add(this.button_copy);
             this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -174,16 +251,23 @@ namespace os_lab_2
 
         #endregion
         private Label label2;
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
+        private Button button_copy;
+        private Button button_move;
+        private Button button_delete;
+        private Button button_file_1;
+        private Button button_folder_2;
         private TextBox textBox1;
         private TextBox textBox2;
         private Panel panel1;
         private Panel panel2;
         private Label label3;
+        private Button button_rename;
+        private Button button_folder_1;
+        private Button button_file_2;
+        private TextBox textBox3;
+        private Label label1;
+        private Button button_exchange;
+        private TextBox textBox4;
     }
 }
 
--- os_lab_2/Form1.cs
+++ os_lab_2/Form1.cs
@@ -10,50 +10,107 @@ namespace os_lab_2
         public Form1()
         {
             InitializeComponent();
-
         }
 
-        private void button1_Click(object sender, EventArgs e) // Copy button
+        private void button_copy_click(object sender, EventArgs e) // Copy button
         {
             try
             {
-                string file_path_src = this.textBox1.Text;
-                string file_name = Path.GetFileName(file_path_src);
-                string file_path_dest = this.textBox2.Text + "\\" + file_name;
-                File.Copy(file_path_src, file_path_dest, true);
+                if (is_file(this.textBox1.Text)) // copy a file
+                {
+                    string file_path_src = this.textBox1.Text;
+                    string file_name = Path.GetFileName(file_path_src);
+                    string file_path_dest = Path.Combine(this.textBox2.Text, file_name);
+                    File.Copy(file_path_src, file_path_dest, true);
+                }
+                else // copy a folder & it's contents
+                {
+                    // create new dir
+                    string dir_src = this.textBox1.Text;
+                    string dir_dest = Path.Combine(this.textBox2.Text, Path.GetFileName(this.textBox1.Text));
+                    DirectoryInfo source = new DirectoryInfo(dir_src);
+                    DirectoryInfo destination = new DirectoryInfo(dir_dest);
+                    copy_dir_recursive(source, destination);
+                }
             }
             catch (Exception ex) // purely for debugging purposes
             {
                 this.stoopid();
             }
         }
-        private void button2_Click(object sender, EventArgs e) // Move button
+        public static void copy_dir_recursive(DirectoryInfo source, DirectoryInfo target) // It just works...
+        {
+            // recursive break
+            if (source.FullName.ToLower() == target.FullName.ToLower()) { return; }
+
+            // check if the target directory exists, otherwise create it.
+            if (Directory.Exists(target.FullName) == false)
+            {
+                Directory.CreateDirectory(target.FullName);
+            }
+
+            // copy each file into new directory
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.ToString(), fi.Name), true);
+            }
+
+            // copy each subdirectory using recursion.
+            foreach (DirectoryInfo sub_dirs in source.GetDirectories())
+            {
+                DirectoryInfo sub_dir = target.CreateSubdirectory(sub_dirs.Name);
+                copy_dir_recursive(sub_dirs, sub_dir);
+            }
+        }
+        private void button_move_click(object sender, EventArgs e) // Move button
         {
             try
             {
+                // Move === Copy & Delete(src)
                 string file_path_src = this.textBox1.Text;
-                string file_name = Path.GetFileName(file_path_src);
-                string file_path_dest = this.textBox2.Text + "\\" + file_name;
-                File.Copy(file_path_src, file_path_dest, true);
-                File.Delete(file_path_src);
+                if (is_file(file_path_src))
+                {
+                    string file_name = Path.GetFileName(file_path_src);
+                    string file_path_dest = Path.Combine(this.textBox2.Text, file_name);
+                    File.Copy(file_path_src, file_path_dest, true);
+                    File.Delete(file_path_src);
+                }
+                else
+                {
+                    string dir_src = this.textBox1.Text;
+                    string dir_dest = Path.Combine(this.textBox2.Text, Path.GetFileName(this.textBox1.Text));
+                    DirectoryInfo source = new DirectoryInfo(dir_src);
+                    DirectoryInfo destination = new DirectoryInfo(dir_dest);
+                    copy_dir_recursive(source, destination);
+                    Directory.Delete(file_path_src, true); // delete a directory & it's contents
+                }
             }
             catch (Exception ex) // purely for debugging purposes
             {
                 this.stoopid();
             }
         }
-        private void button3_Click(object sender, EventArgs e) // Del button
+        private void button_delete_click(object sender, EventArgs e) // Del button
         {
             try
             {
-                File.Delete(this.textBox1.Text);
+                string path = this.textBox1.Text;
+                if (is_file(path))
+                {
+                    File.Delete(path); // delete a file
+                }
+                else
+                {
+                    Directory.Delete(path, true); // delete a directory & it's contents
+                }
             }
             catch
             {
                 this.stoopid();
             }
         }
-        private void button4_Click(object sender, EventArgs e) // Scr button
+        private void button_file_1_click(object sender, EventArgs e) // Scr button
         {
             OpenFileDialog file_dialog = new OpenFileDialog();
             
@@ -65,17 +122,66 @@ namespace os_lab_2
                 this.textBox1.Text = file_dialog.FileName;
             }
         }
-        private void button5_Click(object sender, EventArgs e) // Dest button
+        private void button_file_2_click(object sender, EventArgs e) // Scr button
+        {
+            OpenFileDialog file_dialog = new OpenFileDialog();
+            
+            file_dialog.InitialDirectory = this.textBox2.Text;
+            file_dialog.RestoreDirectory = true;
+            
+            if (file_dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox2.Text = file_dialog.FileName;
+            }
+        }
+        private void button_folder_1_click(object sender, EventArgs e) // Dest button
         {
             FolderBrowserDialog folder_dialog = new FolderBrowserDialog();
-            
-//            folder_dialog.InitialDirectory = this.textBox1.Text;
-//            folder_dialog.RestoreDirectory = true;
-            
+            if (folder_dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = folder_dialog.SelectedPath;
+            }
+        }
+        private void button_folder_2_click(object sender, EventArgs e) // Dest button
+        {
+            FolderBrowserDialog folder_dialog = new FolderBrowserDialog();
             if (folder_dialog.ShowDialog() == DialogResult.OK)
             {
                 this.textBox2.Text = folder_dialog.SelectedPath;
             }
+        }
+        private void button_rename_click(object sender, EventArgs e) // Rename button
+        {
+            try
+            {
+                string file_path_src = this.textBox1.Text;
+                string file_name = Path.GetDirectoryName(file_path_src);
+                string new_file_name = "";
+                string file_path_dest = Path.Combine(this.textBox2.Text, file_name);
+                File.Copy(file_path_src, file_path_dest, true);
+                File.Delete(file_path_src);
+            }
+            catch (Exception ex) // purely for debugging purposes
+            {
+                this.stoopid();
+            }
+        }
+        private void button_exchange_click(object sender, EventArgs e) // switch paths
+        {
+            string temp = this.textBox1.Text;
+            this.textBox1.Text = this.textBox2.Text;
+            this.textBox2.Text = temp;
+        }
+        private bool is_file(string path)
+        {
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(path);
+
+            //detect whether its a directory or file
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                return false;
+            else
+                return true;
         }
         private void stoopid()
         {
